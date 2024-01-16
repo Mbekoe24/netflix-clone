5b6fd42b88662568bdc1e675c0278732e9c23f6e
MBest24 <mbekoe24@gmail.com>
2024-01-15 19:53:55 -0500

    adding api request & axios

--- netflix-2.0/src/request.js
+++ netflix-2.0/src/request.js
@@ -1,19 +1,16 @@
 const API_KEY = "064429f59b4e4b1e348d7fd678334c15";
 
 
-
-
 const requests = {
-  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
-  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_network=123`,
-  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
-  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
-  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
-  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
-  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
-  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+  getTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  getNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_network=123`,
+  getTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  getActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  getComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  getHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  getRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  getDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
 };
-
 
 
 export default requests;
